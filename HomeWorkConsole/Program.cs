5286aefc4159dea98c7ddc8141d8fcd6a225479d
AlexsandrDowgaluk <Dowgaluk88@gmail.com>
2019-09-02 11:30:36 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkConsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Введите число ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            
+            do
+            {
+
+                if (Num1 == 0)
+                {
+                    Console.WriteLine("Число не может быть 0 ");
+                    Console.WriteLine("Введите коректное число ");
+                    Num1 = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+            while (Num1!=0);
+            
+            
+            Console.WriteLine("Введите  второе число ");
+
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+      
+            
+           
+
+          
+
+
+      
+
